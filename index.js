a38ab1a8a31a5482a5cb9028a11e3d0a8852ef43
Elspeth Meakin <elspeth_meakin@hotmail.co.uk>
2023-02-27 13:26:23 +0000

    draft get functions added to employee class

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const Employee = require("./lib/Employee")
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -9,31 +10,3 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
-
-function newEmployee() {
-    inquirer.prompt({
-        type: 'list',
-        name: 'employeeType',
-        message: 'What type of employee do you want to add?',
-        choices: ['Manager', 'Engineer', 'Intern'],
-        validate: answer => {
-            if (answer === "") {
-                return "Employee tye required"
-            }
-            else { return true }
-        }
-    })
-    .then((answer) => {
-        if (answer.employeeType === 'Manager') {
-            console.log("You chose Manager!")
-        }
-        if (answer.employeeType === "Engineer") {
-            console.log("You chose Engineer!")
-        }
-        if (answer.employeeType === "Intern") {
-            console.log("You chose Intern!")
-        }
-    })
-}
-
-newEmployee()
