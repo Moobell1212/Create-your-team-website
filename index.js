2509ac9c301b87feb2c3c0d6e11a12237e77372c
Elspeth Meakin <elspeth_meakin@hotmail.co.uk>
2023-02-28 12:24:06 +0000

    employee questions moved to index

--- index.js
+++ index.js
@@ -12,8 +12,69 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./src/page-template.js");
 
 function startTeamBuild() {
-    Manager
-    const manager = new Employee (this.name, this.id, this.email, 'Manager')
+    inquirer.prompt({
+        type: 'list',
+        name: 'employeeType',
+        message: 'What type of employee do you want to add?',
+        choices: ['Manager', 'Engineer', 'Intern', 'Finish building team'],
+    })
+        .then((answer) => {
+            if (answer.employeeType === 'Manager') {
+                console.log("You chose Manager!")
+            }
+            if (answer.employeeType === "Engineer") {
+                console.log("You chose Engineer!")
+            }
+            if (answer.employeeType === "Intern") {
+                console.log("You chose Intern!")
+            }
+            else {
+                console.log("You have finished building the team!")
+            }
+        })
 };
+
+function askQuestions() {
+    inquirer.prompt([{
+        type: 'input',
+        name: 'name',
+        message: 'What is the employee name?',
+        validate: answer => {
+            if (answer === "") {
+                return "Employee name required"
+            }
+            else { return true }
+        }
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is the employee id?',
+        validate: answer => {
+            if (answer === "") {
+                return "Employee id required"
+            }
+            else { return true }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is the employee's email address?",
+        validate: answer => {
+            if (!emailCheck.validate(answer)) {
+                return "A valid email is required"
+            }
+            else {
+                return true
+            }
+        }
+    }]
+
+
+    )
+
+
+}
 
 startTeamBuild()
